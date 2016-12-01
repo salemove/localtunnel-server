7896ba28b881cc6893aed118b79f972e01e4b208
Indrek Juhkam <indrek@urgas.eu>
2016-12-01 18:08:56 +0200

    Use Rx for better socket tracking

--- src/Tunnel.js
+++ src/Tunnel.js
@@ -3,22 +3,25 @@ import Debug from 'debug';
 import on_finished from 'on-finished';
 import BindingAgent from './BindingAgent';
 import http from 'http';
+import Rx from 'rxjs/Rx';
+import R from 'ramda';
 
 const logError = new Debug('localtunnel:server:error');
 
 export default function createTunnel(id, {endCallback, startCallback}) {
   let sockets = [];
-  let waiting = [];
   const debug = new Debug(`localtunnel:server:${id}`);
 
   // new tcp server to service requests for this client
   const server = net.createServer();
 
-  // track initial user connection setup
-  let conn_timeout;
+  const socketsChange = new Rx.ReplaySubject(1);
+  const newSockets = new Rx.Subject();
+  const serverClose = new Rx.Subject();
+  const requests = new Rx.Subject();
 
-  server.on('close', _cleanup);
-  server.on('connection', _handle_socket);
+  server.on('close', () => serverClose.next());
+  server.on('connection', socket => newSockets.next(socket));
 
   server.on('error', function(err) {
     // where do these errors come from?
@@ -38,81 +41,76 @@ export default function createTunnel(id, {endCallback, startCallback}) {
     startCallback({port: port, max_conn_count: 10});
   });
 
-  _maybe_destroy();
+  newSockets.subscribe(handleSocket);
 
-  function _maybe_destroy() {
-    clearTimeout(conn_timeout);
-    conn_timeout = setTimeout(function() {
-      // sometimes the server is already closed but the event has not fired?
-      try {
-        clearTimeout(conn_timeout);
-        server.close();
-      } catch (err) {
-        _cleanup();
-      }
-    }, 5000);
-  }
+  const inactivityTimer = Rx.Observable.timer(5000);
 
-  // new socket connection from client for tunneling requests to client
-  function _handle_socket(socket) {
+  const activity = socketsChange.filter(sockets => sockets.length !== 0);
+  const inactivity = socketsChange.startWith([]).filter(sockets => sockets.length === 0)
+    .flatMap(() => inactivityTimer.takeUntil(activity));
+
+  const getActiveSocket = socketsChange
+    .map(R.filter(R.propEq('active', true)))
+    .filter(sockets => sockets.length > 0)
+    .map(R.last)
+    .take(1);
+
+  const stop = Rx.Observable.merge(inactivity, serverClose).take(1);
+
+  stop.subscribe(() => {
+    debug('closed tcp socket for client');
+    try {
+      server.close();
+    } catch (err) {}
+    endCallback();
+  });
+
+  const markSocketAsInactive = socket => {
+    socket.active = false;
+    socketsChange.next(sockets);
+  };
+
+  const markSocketAsActive = socket => {
+    socket.active = true;
+    socketsChange.next(sockets);
+  };
+
+  requests
+    .takeUntil(stop)
+    .flatMap(request =>
+      getActiveSocket
+        .do(markSocketAsInactive)
+        .map(socket => ({request, socket})))
+    .subscribe(
+      ({request, socket}) =>
+        request(socket).then(() => markSocketAsActive(socket)),
+      err => logError('Error forwarding request', err)
+    );
+
+  function handleSocket(socket) {
     debug('new connection from: %s:%s', socket.address().address, socket.address().port);
 
-    // a single connection is enough to keep client id slot open
-    clearTimeout(conn_timeout);
-
     socket.once('close', function(had_error) {
-      debug('closed socket (error: %s)', had_error);
+      sockets = R.reject(s => s === socket, sockets);
+      socketsChange.next(sockets);
 
-      // what if socket was servicing a request at this time?
-      // then it will be put back in available after right?
-      // we need a list of sockets servicing requests?
-
-      // remove this socket
-      const idx = sockets.indexOf(socket);
-      if (idx >= 0) {
-        sockets.splice(idx, 1);
-      }
-
-      // need to track total sockets, not just active available
-      debug('remaining client sockets: %s', sockets.length);
-
-      // no more sockets for this ident
-      if (sockets.length === 0) {
-        debug('all sockets disconnected');
-        _maybe_destroy();
-      }
+      debug('closed socket (error: %s, remaining: %s)', had_error, sockets.length);
     });
 
     // close will be emitted after this
     socket.on('error', function(err) {
-      // we don't log here to avoid logging crap for misbehaving clients
+      debug('connection error: %s', err);
       socket.destroy();
     });
 
-    sockets.push(socket);
-    _process_waiting();
-  }
-
-  function _process_waiting() {
-    const wait_cb = waiting.shift();
-    if (wait_cb) {
-      debug('handling queued request');
-      next_socket(wait_cb);
-    }
-  }
-
-  function _cleanup() {
-    debug('closed tcp socket for client');
-
-    clearTimeout(conn_timeout);
-
-    // clear waiting by ending responses, (requests?)
-    waiting.forEach(handler => handler(null));
-
-    endCallback();
+    socket.active = true;
+    sockets = R.append(socket, sockets);
+    socketsChange.next(sockets);
   }
 
   function forwardHTTPRequest(req, res) {
+    debug('processing http request');
+
     let finished = false;
     // flag if we already finished before we get a socket
     // we can't respond to these requests
@@ -121,25 +119,9 @@ export default function createTunnel(id, {endCallback, startCallback}) {
       req.connection.destroy();
     });
 
-    next_socket(async socket => {
+    requests.next(socket => {
       // the request already finished or tunnel disconnected
-      if (finished) return;
-
-      // happens when client upstream is disconnected (or disconnects)
-      // and the proxy iterates the waiting list and clears the callbacks
-      // we gracefully inform the user and kill their conn
-      // without this, the browser will leave some connections open
-      // and try to use them again for new requests
-      // we cannot have this as we need bouncy to assign the requests again
-      // TODO(roman) we could instead have a timeout above
-      // if no socket becomes available within some time,
-      // we just tell the user no resource available to service request
-      if (!socket) {
-        res.statusCode = 504;
-        res.end();
-        req.connection.destroy();
-        return;
-      }
+      if (finished) return Promise.resolve({});
 
       const agent = new BindingAgent({socket: socket});
 
@@ -150,7 +132,7 @@ export default function createTunnel(id, {endCallback, startCallback}) {
         headers: req.headers
       };
 
-      await new Promise(resolve => {
+      return new Promise(resolve => {
         // what if error making this request?
         const client_req = http.request(opt, function(client_res) {
           // write response code and headers
@@ -176,30 +158,17 @@ export default function createTunnel(id, {endCallback, startCallback}) {
   }
 
   function forwardSocket(req, sock, head) {
+    debug('processing socket request');
+
     let finished = false;
     sock.once('end', () => {
       finished = true;
     });
 
-    next_socket(async socket => {
+    requests.next(socket => {
       // the request already finished or tunnel disconnected
       if (finished)
-        return;
-
-      // happens when client upstream is disconnected (or disconnects)
-      // and the proxy iterates the waiting list and clears the callbacks
-      // we gracefully inform the user and kill their conn
-      // without this, the browser will leave some connections open
-      // and try to use them again for new requests
-      // we cannot have this as we need bouncy to assign the requests again
-      // TODO(roman) we could instead have a timeout above
-      // if no socket becomes available within some time,
-      // we just tell the user no resource available to service request
-      if (!socket) {
-        sock.destroy();
-        req.connection.destroy();
-        return;
-      }
+        return Promise.resolve({});
 
       // websocket requests are special in that we simply re-create the header info
       // and directly pipe the socket data
@@ -215,43 +184,10 @@ export default function createTunnel(id, {endCallback, startCallback}) {
       socket.pipe(sock).pipe(socket);
       socket.write(arr.join('\r\n'));
 
-      await new Promise(resolve => {
+      return new Promise(resolve => {
         socket.once('end', resolve);
       });
     });
-  }
-
-  function next_socket(handler) {
-    // socket is a tcp connection back to the user hosting the site
-    const sock = sockets.shift();
-
-    if (!sock) {
-      debug('no more client, queue callback');
-      waiting.push(handler);
-      return;
-    }
-
-    const onRequestProcessed = () => {
-      if (!sock.destroyed) {
-        debug('retuning socket');
-        sockets.push(sock);
-      }
-
-      // no sockets left to process waiting requests
-      if (sockets.length === 0) {
-        return;
-      }
-
-      _process_waiting();
-    };
-
-    debug('processing request');
-    handler(sock)
-      .then(onRequestProcessed)
-      .catch(err => {
-        logError(err);
-        onRequestProcessed();
-      });
   }
 
   return {forwardHTTPRequest, forwardSocket};
